df9a366c011f7fddeb72ea62d1d7685e5ca51683
chenzhiyuan <chenzhiyuan@powerun.org.cn>
2018-04-17 11:36:46 +0800

    less-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,44 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+	     rules: [
+	     	{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+
+                }
+            },
+            {
+	            test: /\.less$/,
+	            use: [{
+	                loader: "style-loader" // creates style nodes from JS strings
+	            }, {
+	                loader: "css-loader" // translates CSS into CommonJS
+	            }, {
+	                loader: "less-loader" // compiles Less to CSS
+	            }]
+	        },
+			{
+		        test: /\.css$/,
+		        use: [
+		            'style-loader',
+		            'css-loader'
+	        	]
+	        },
+	     ]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'use plugin',
+			filename: 'index.html'
+		})
+	]
 };
